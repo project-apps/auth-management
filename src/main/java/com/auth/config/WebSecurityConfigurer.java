c6be9cde653f1e107ca9eb6c5db8c46eb4212055
mishrarajan <mishra.k.rajan@gmail.com>
2020-05-04 08:39:24 +0530

    Profiles

--- src/main/java/com/auth/config/WebSecurityConfigurer.java
+++ src/main/java/com/auth/config/WebSecurityConfigurer.java
@@ -4,10 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +23,8 @@ import com.auth.service.CustomUserDetailsService;
 import com.auth.service.OAuth2AuthenticationFailureHandler;
 import com.auth.service.OAuth2AuthenticationSuccessHandler;
 
-@EnableWebSecurity
+//@EnableWebSecurity
+@Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 	private static final Log logger = LogFactory.getLog(WebSecurityConfigurer.class);
 	@Autowired
